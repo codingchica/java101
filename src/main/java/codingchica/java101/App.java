f4df381401722f4e5587f4484adc29abc89de16d
= <>
2023-06-18 20:41:42 -0500

    Hello gorgeous!

--- src/main/java/codingchica/java101/App.java
+++ src/main/java/codingchica/java101/App.java
@@ -2,12 +2,22 @@ package codingchica.java101;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /**
+     * Retrieve the greeting that should be outputted when the application is run.
+     * @return The greeting to output.
+     */
+    public String getGreeting() {
+        return "Hello, gorgeous!";
+    }
+
+    /**
+     * The main entry-point into the application.
+     * @param args Command-line arguments - not currently used.
+     */
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println(app.getGreeting());
     }
 }
--- src/test/java/codingchica/java101/AppTest.java
+++ src/test/java/codingchica/java101/AppTest.java
@@ -1,20 +1,32 @@
 package codingchica.java101;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
     /**
-     * Rigorous Test :-)
+     * Unit tests for the getGreeting method.
+     * @see App#getGreeting()
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    @Nested
+  class GetGreetingTest {
+      @Test
+      void getGreeting_whenInvoked_thenExpectedResultReturned() {
+          // Setup
+          String expectedGreeting = "Hello, gorgeous!";
+          App app = new App();
+
+          // Execution
+          String actualGreeting = app.getGreeting();
+
+          // Validation
+          assertEquals(expectedGreeting, actualGreeting, "greeting");
+      }
+  }
 }
