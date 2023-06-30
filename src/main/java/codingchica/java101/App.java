87fbc7b2326638b55332bf37143004bca3c67fa6
= <>
2023-06-30 15:41:35 -0500

    Style Matters!  Adding a Checkstyle Build Breaker

--- src/main/java/codingchica/java101/App.java
+++ src/main/java/codingchica/java101/App.java
@@ -11,7 +11,7 @@ public class App {
      * @param byteValue2 The second value to add.
      * @return An int value representing the two values added together.
      */
-    public static int add(byte byteValue1, byte byteValue2) {
+    public static int add(final byte byteValue1, final byte byteValue2) {
         return byteValue1 + byteValue2;
     }
 
@@ -22,7 +22,7 @@ public class App {
      * @param shortValue2 The second value to add.
      * @return An int value representing the two values added together.
      */
-    public static int add(short shortValue1, short shortValue2) {
+    public static int add(final short shortValue1, final short shortValue2) {
         return shortValue1 + shortValue2;
     }
 
@@ -33,12 +33,16 @@ public class App {
      * @param intValue2 The second value to add.
      * @return An int value representing the two values added together.
      */
-    public static int add(int intValue1, int intValue2) {
+    public static int add(final int intValue1, final int intValue2) {
         long sum = (long) intValue1 + (long) intValue2;
         if (sum < Integer.MIN_VALUE) {
-            throw new IllegalArgumentException(String.format("int underflow with addends:  %s and %s", intValue1, intValue2));
+            throw new IllegalArgumentException(String.format(
+                    "int underflow with addends:  %s and %s", intValue1,
+                    intValue2));
         } else if (sum > Integer.MAX_VALUE) {
-            throw new IllegalArgumentException(String.format("int overflow with addends:  %s and %s", intValue1, intValue2));
+            throw new IllegalArgumentException(String.format(
+                    "int overflow with addends:  %s and %s", intValue1,
+                    intValue2));
         }
         return (int) sum;
     }
@@ -46,10 +50,10 @@ public class App {
     /**
      * Constructor for the App class.
      *
-     * @param name The name to use in the application's execution.
+     * @param newName The name to use in the application's execution.
      */
-    public App(String name) {
-        this.name = name;
+    public App(final String newName) {
+        this.name = newName;
     }
 
     /**
@@ -58,7 +62,8 @@ public class App {
     private String name;
 
     /**
-     * Retrieve the greeting that should be outputted when the application is run.
+     * Retrieve the greeting that should be outputted when the application is
+     * run.
      *
      * @return The greeting to output.
      */
@@ -71,7 +76,7 @@ public class App {
      *
      * @param args Command-line arguments - not currently used.
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         App app = new App("Maddie");
         System.out.println(app.getGreeting());
     }
---
+++ src/main/java/codingchica/java101/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Code demonstrating the basics of the Java language.
+ */
+package codingchica.java101;
