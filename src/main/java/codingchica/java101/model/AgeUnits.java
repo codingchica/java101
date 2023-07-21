1540fdfba49d98cbddd5ec6c61cdcb6abbbc8bf7
= <>
2023-07-20 20:54:13 -0500

    What Are My Options? A Simple Java Enum

---
+++ src/main/java/codingchica/java101/model/AgeUnits.java
@@ -0,0 +1,20 @@
+package codingchica.java101.model;
+
+
+/**
+ * The time units we will support for age retrieval.
+ */
+public enum AgeUnits {
+    /**
+     * Age should be measured in years.
+     */
+    YEARS,
+    /**
+     * Age should be measured in days.
+     */
+    DAYS,
+    /**
+     * Age should be measured in hours.
+     */
+    HOURS;
+}
